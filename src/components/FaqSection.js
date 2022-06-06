6aac46135f714b33c9104a5fa53cf1fa870f8f24
Gilles Elan <gilles.muess@elan-formation.fr>
2022-06-06 11:52:21 +0200

    Added pages and section for about us

---
+++ src/components/FaqSection.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+function FaqSection() {
+  return (
+    <div className='faq'>
+      <div className='description'>
+        <h2>
+          Any Questions ? <span>FAQ</span>
+        </h2>
+        <div className='question'>
+          <h4>How do I start ?</h4>
+          <div className='answer'>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit ?</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Repellendus amet eligendi temporibus perspiciatis laudantium totam
+              reiciendis labore neque quibusdam officia at libero maxime beatae
+              magnam veritatis, aliquid debitis pariatur sed.
+            </p>
+          </div>
+        </div>
+        <div className='question'>
+          <h4>Payment methods</h4>
+          <div className='answer'>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit ?</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Repellendus amet eligendi temporibus perspiciatis laudantium totam
+              reiciendis labore neque quibusdam officia at libero maxime beatae
+              magnam veritatis, aliquid debitis pariatur sed.
+            </p>
+          </div>
+        </div>
+        <div className='question'>
+          <h4>What products do you offer ?</h4>
+          <div className='answer'>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit ?</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Repellendus amet eligendi temporibus perspiciatis laudantium totam
+              reiciendis labore neque quibusdam officia at libero maxime beatae
+              magnam veritatis, aliquid debitis pariatur sed.
+            </p>
+          </div>
+        </div>
+        <div className='question'>
+          <h4>How do you work ?</h4>
+          <div className='answer'>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit ?</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Repellendus amet eligendi temporibus perspiciatis laudantium totam
+              reiciendis labore neque quibusdam officia at libero maxime beatae
+              magnam veritatis, aliquid debitis pariatur sed.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FaqSection;
